62a38118ac6fefc37886b647838d724762ab0ad3
andregra <andregra717@gmail.com>
2022-02-03 16:32:20 +0100

    tailwind config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState} from "react";
 
-function App() {
+const  App = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen p-10 text-center">
+      <h1 className="text-4xl">A React Codepen Template with tailwind CSS</h1>
+      <p className="text-xl py-6">
+        You clicked the button <strong>{count}</strong> times.
+      </p>
+      <button
+        className="bg-yellow-200 font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider text-red-600 focus:outline-none"
+        onClick={() => setCount(count + 1)}
+      >
+        Click
+      </button>
     </div>
   );
-}
+};
 
 export default App;
